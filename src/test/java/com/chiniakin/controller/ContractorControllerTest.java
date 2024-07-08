6adc4b8f62fa6f9b9728feb776d7c42339402d37
ChiniakinD <pilotuiga@gmail.com>
2024-07-08 16:36:29 +0300

    test(task): Добавление тестов для контроллеров.
    Для ContractorController, CountryController, IndustryController, OrgFormController добавлены тесты.

---
+++ src/test/java/com/chiniakin/controller/ContractorControllerTest.java
@@ -0,0 +1,86 @@
+package com.chiniakin.controller;
+
+import com.chiniakin.model.ContractorModel;
+import com.chiniakin.model.CountryResponse;
+import com.chiniakin.model.IndustryResponse;
+import com.chiniakin.model.OrgFormResponse;
+import com.chiniakin.service.interfaces.ContractorService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.Collections;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ContractorController.class)
+@AutoConfigureMockMvc
+public class ContractorControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ContractorService contractorService;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    void getAllContractorsReturnListOfCountries() throws Exception {
+        ContractorModel contractorModel = getContractorModel();
+
+        Page<ContractorModel> contractorPage = new PageImpl<>(Collections.singletonList(contractorModel),
+                PageRequest.of(0, 1), 6);
+
+        when(contractorService.search(0, 10)).thenReturn(contractorPage);
+        mockMvc.perform(MockMvcRequestBuilders.get("/contractors/search")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .param("page", "0")
+                        .param("size", "10"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(contractorPage)));
+    }
+
+    @Test
+    void getContractorByIdReturnValidContractor() throws Exception {
+        ContractorModel contractorModel = getContractorModel();
+        when(contractorService.getContractorById("1")).thenReturn(contractorModel);
+        mockMvc.perform(MockMvcRequestBuilders.get("/contractors/get/{id}", "1"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(contractorModel)));
+    }
+
+    private static ContractorModel getContractorModel() {
+        ContractorModel contractorModel = new ContractorModel();
+        contractorModel.setId("1");
+        contractorModel.setName("Рога и копыта");
+        contractorModel.setInn("1234567890");
+        contractorModel.setOgrn("1234567890");
+        contractorModel.setParentId("1");
+        contractorModel.setNameFull("ООО Рога и копыта");
+        contractorModel.setCountryResponse(new CountryResponse());
+        contractorModel.setIndustryResponse(new IndustryResponse());
+        contractorModel.setOrgFormResponse(new OrgFormResponse());
+        contractorModel.setActive(true);
+        return contractorModel;
+    }
+
+    @Test
+    void deleteContractorShouldSuccessfulDeleteContractor() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/contractors/delete/{id}", "1"))
+                .andExpect(status().isOk());
+        Mockito.verify(contractorService).deleteContractorById("1");
+    }
+}
---
+++ src/test/java/com/chiniakin/controller/IndustryControllerTest.java
@@ -0,0 +1,77 @@
+package com.chiniakin.controller;
+
+import com.chiniakin.model.IndustryModel;
+import com.chiniakin.service.interfaces.IndustryService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(IndustryController.class)
+public class IndustryControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private IndustryService industryService;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    public void getAllIndustryShouldReturnListOfIndustries() throws Exception {
+        List<IndustryModel> industries = Arrays.asList(
+                new IndustryModel(1L, "Воздушный транспорт", true),
+                new IndustryModel(2L, "Водный транспорт", true)
+        );
+        when(industryService.getAllIndustries()).thenReturn(industries);
+        mockMvc.perform(get("/industries/"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").value("1"))
+                .andExpect(jsonPath("$[0].name").value("Воздушный транспорт"))
+                .andExpect(jsonPath("$[1].id").value("2"))
+                .andExpect(jsonPath("$[1].name").value("Водный транспорт"));
+    }
+
+    @Test
+    public void getIndustryByIdReturnValidIndustry() throws Exception {
+        Long id = 1L;
+        IndustryModel industryModel = new IndustryModel(id, "Воздушный транспорт", true);
+        when(industryService.getIndustryById(id)).thenReturn(industryModel);
+        mockMvc.perform(get("/industries/get/{id}", id))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(id))
+                .andExpect(jsonPath("$.name").value("Воздушный транспорт"));
+    }
+
+    @Test
+    public void updateIndustryShouldSuccessfulUpdateIndustry() throws Exception {
+        Long id = 1L;
+        IndustryModel industryModel = new IndustryModel(id, "Воздушный транспорт", true);
+        doNothing().when(industryService).updateIndustry(id, industryModel);
+        mockMvc.perform(put("/industries/add/{id}", id)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(industryModel)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void deleteCountryShouldSuccessfulDeleteCountry() throws Exception {
+        Long id = 1L;
+        doNothing().when(industryService).deleteIndustryById(id);
+        mockMvc.perform(delete("/industries/delete/{id}", id))
+                .andExpect(status().isOk());
+    }
+}
